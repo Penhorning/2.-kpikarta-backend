a78abc5f3364626707d13789043c0abef75af436
sachinots <sachin.kumar@otssolutions.com>
2022-06-22 19:29:15 +0530

    change redirect web url in custom social routes

--- src/server/boot/customRoutes/socialRoutes.js
+++ src/server/boot/customRoutes/socialRoutes.js
@@ -13,10 +13,10 @@ module.exports = function socialRoutes(app) {
     }
 
     if (req.user.emailVerified && req.user.currentPlan) {
-        res.redirect(`${process.env.WEB_LOGIN_URL}?name=${user.name}&email=${user.email}&userId=${user.userId}&access_token=${user.accessToken}` );
+        res.redirect(`${process.env.WEB_URL}/login?name=${user.name}&email=${user.email}&userId=${user.userId}&access_token=${user.accessToken}` );
     } else {
         req.user.updateAttributes({emailVerified: true}, (err)=>{
-            res.redirect(`${process.env.AUTH_REDIRECT_URL}?name=${user.name}&email=${user.email}&userId=${user.userId}&access_token=${user.accessToken}`);
+            res.redirect(`${process.env.WEB_URL}/sign-up?name=${user.name}&email=${user.email}&userId=${user.userId}&access_token=${user.accessToken}`);
         });
     }
 
