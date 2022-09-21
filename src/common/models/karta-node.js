4cec57f512fcdd7be9845ee895a27429a2f70203
sachin.kumar <sachin.kumar@otssolutions.com>
2022-09-21 19:37:46 +0530

    percentage and kpi stats done in - my kpis

--- src/common/models/karta-node.js
+++ src/common/models/karta-node.js
@@ -16,12 +16,6 @@ module.exports = function (Kartanode) {
     }
   }
 
-  // const addDays = (date, days) => {
-  //     let result = new Date(date);
-  //     result.setDate(result.getDate() + days);
-  //     return result;
-  // }
-
   const KARTA_LOOKUP = {
     $lookup: {
       from: 'karta',
@@ -38,8 +32,27 @@ module.exports = function (Kartanode) {
 
 
 /* =============================CUSTOM METHODS=========================================================== */
+  // Get kpi status by contributor's userId
+  Kartanode.kpiStats = (userId, next) => {
+    let completedQuery = { "contributors.userId": userId, $expr: { $eq: [ { "$arrayElemAt": ["$target.value", 0] }, "$achieved_value" ] } };
+    let inCompletedQuery = { "contributors.userId": userId, $expr: { $gt: [ { "$arrayElemAt": ["$target.value", 0] }, "$achieved_value" ] } };
+
+    Kartanode.count(completedQuery, (err, result) => {
+      Kartanode.count(inCompletedQuery, (err2, result2) => {
+        Kartanode.count({ "contributors.userId": userId }, (err2, result3) => {
+          let data = {
+            "All": result3,
+            "InProgress": result2,
+            "Completed": result
+          }
+          next(err2, data);
+        });
+      });
+    });
+  }
+
   // Get kpi nodes by contributor's userId
-  Kartanode.kpiNodes = (page, limit, searchQuery, userId, kpiType, sortBy, percentage, targetTypes, startUpdatedDate, endUpdatedDate, next) => {
+  Kartanode.kpiNodes = (page, limit, searchQuery, userId, kpiType, sortBy, percentage, targetTypes, startUpdatedDate, endUpdatedDate, startDueDate, endDueDate, next) => {
     page = parseInt(page, 10) || 1;
     limit = parseInt(limit, 10) || 100;
 
@@ -52,14 +65,6 @@ module.exports = function (Kartanode) {
     if (kpiType === "shared") query = { "sharedTo.userId": userId };
     else query = { "contributors.userId": userId };
 
-    // endUpdatedDate = addDays(endUpdatedDate, 1);
-    // if (startUpdatedDate && endUpdatedDate) {
-    //   query.updatedAt = {
-    //       $gte: new Date(startUpdatedDate),
-    //       $lte: endUpdatedDate
-    //   }
-    // }
-
     // Filter nodes by last updated date ranges
     if (startUpdatedDate && endUpdatedDate) {
       query.updatedAt = {
@@ -67,13 +72,27 @@ module.exports = function (Kartanode) {
         $lte: moment(endUpdatedDate).toDate()
       }
     }
+    // Filter nodes by due date ranges
+    if (startDueDate && endDueDate) {
+      query.due_date = {
+        $gte: moment(startDueDate).toDate(),
+        $lte: moment(endDueDate).toDate()
+      }
+    }
     // Filter nodes by frequency
     if (targetTypes.length > 0) {
       query["target.0.frequency"] = { $in: targetTypes }
     }
     // Filter nodes by percentage
+    let percentage_query = {};
     if (percentage) {
-      query["target.0.percentage"] = { $gte: percentage.min, $lte: percentage.max }
+      let percentageRange = [];
+      percentage.forEach(item => {
+        percentageRange.push({
+          "target.0.percentage": { $gte: item.min, $lte: item.max }
+        });
+      });
+      percentage_query = { $or: percentageRange };
     }
 
     // Sort nodes by date and percentage
@@ -102,11 +121,32 @@ module.exports = function (Kartanode) {
           $match: query
         },
         {
+          $match: percentage_query
+        },
+        {
           $sort: SORT
         },
         SEARCH_MATCH,
         KARTA_LOOKUP,
         UNWIND_KARTA,
+        {
+          $lookup: {
+            from: "user",
+            localField: "karta.userId",
+            foreignField: "_id",
+            pipeline: [
+              { $project: {
+                  "fullName": "$fullName",
+                  "email": "$email"
+                } 
+              }
+            ],
+            as: "karta.user",
+          }
+        },
+        {
+          $unwind: "$karta.user"
+        },
         {
           $facet: {
             metadata: [{ $count: "total" }, { $addFields: { 'page': page } }],
@@ -114,11 +154,12 @@ module.exports = function (Kartanode) {
           }
         }
       ]).toArray((err, result) => {
-        result[0].data.length > 0 ? result[0].metadata[0].count = result[0].data.length : 0;
+        if (result) result[0].data.length > 0 ? result[0].metadata[0].count = result[0].data.length : 0;
         next(err, result);
       });
     });
   }
+
 
 /* =============================REMOTE HOOKS=========================================================== */
   // Include childrens when fetching nodes by kartaId
