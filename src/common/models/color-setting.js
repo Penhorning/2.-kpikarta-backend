e9e71630cdf0c2de9c3de0b8ddbacc49ef9762e0
sachin.kumar <sachin.kumar@otssolutions.com>
2022-11-10 16:22:59 +0530

    add color setting method for karta

--- src/common/models/color-setting.js
+++ src/common/models/color-setting.js
@@ -1,11 +1,23 @@
 'use strict';
 
 module.exports = function(Colorsetting) {
+    // Get color settings by user
     Colorsetting.getColorsByUser = (userId, next) => {
         Colorsetting.findOne({ where: { userId } }, function (err, userResult) {
             if (userResult) next(err, userResult.color_settings);
             else {
                 Colorsetting.findOne({ where: { "userId" : { "exists" : false } } }, function (error, globalResult) {
+                    next(error, globalResult.color_settings);
+                });
+            }
+        });
+    }
+    // Get color settings by karta
+    Colorsetting.getColorsByKarta = (userId, kartaId, next) => {
+        Colorsetting.findOne({ where: { userId, kartaId } }, function (err, kartaResult) {
+            if (kartaResult) next(err, kartaResult.color_settings);
+            else {
+                Colorsetting.findOne({ where: { "userId" : { "exists" : false }, "kartaId" : { "exists" : false } } }, function (error, globalResult) {
                     next(error, globalResult.color_settings);
                 });
             }
