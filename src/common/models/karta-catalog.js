fdafa84ec8c80d490b3d45603656ce2fe1515c16
sachin.kumar <sachin.kumar@otssolutions.com>
2022-12-02 20:11:17 +0530

    add folder making script in server.js

--- src/common/models/karta-catalog.js
+++ src/common/models/karta-catalog.js
@@ -7,8 +7,8 @@ module.exports = function(Kartacatalog) {
     const SORT = {
         $sort: { createdAt: -1 }
     }
-    // User lookup with id
-    const USER_LOOKUP_WITH_ID = (userId) => {
+    // User lookup
+    const USER_LOOKUP = (userId) => {
         return {
             $lookup: {
                 from: "user",
@@ -19,28 +19,6 @@ module.exports = function(Kartacatalog) {
                   { 
                     $match: { 
                       $expr: { $eq: ["$_id", "$$user_id"] }
-                    } 
-                  },
-                  {
-                    $project: { "fullName": 1, "email": 1 }
-                  }
-                ],
-                as: "user"
-            }
-        }
-    }
-    // User lookup with email
-    const USER_LOOKUP_WITH_EMAIL = (email) => {
-        return {
-            $lookup: {
-                from: "user",
-                let: {
-                    user_email: email
-                },
-                pipeline: [
-                  { 
-                    $match: { 
-                      $expr: { $eq: ["$email", "$$user_email"] }
                     } 
                   },
                   {
@@ -68,15 +46,16 @@ module.exports = function(Kartacatalog) {
 
 /* =============================CUSTOM METHODS=========================================================== */
   // Get all catalogs
-  Kartacatalog.getAll = (userId, searchQuery, page, limit, nodeTypes, next) => {
+  Kartacatalog.getAll = (userId, searchQuery, type, page, limit, nodeTypes, next) => {
     page = parseInt(page, 10) || 1;
     limit = parseInt(limit, 10) || 100;
 
     let search_query = searchQuery ? searchQuery.trim().replace(/[.*+?^${}()|[\]\\]/g, '\\$&') : "";
 
-    userId = Kartacatalog.getDataSource().ObjectID(userId);
+    let objectUserId = Kartacatalog.getDataSource().ObjectID(userId);
 
-    let query = { "userId": userId, "is_deleted": false };
+    let query = { "userId": objectUserId, "is_deleted": false };
+    if (type === "shared") query = { "sharedTo.userId": userId, "is_deleted": false }
 
     // Filter catalogs
     if (nodeTypes && nodeTypes.length > 0) {
@@ -103,7 +82,7 @@ module.exports = function(Kartacatalog) {
           $match: query
         },
         SEARCH_MATCH,
-        USER_LOOKUP_WITH_ID(userId),
+        USER_LOOKUP(objectUserId),
         UNWIND_USER,
         SORT,
         FACET(page, limit)
@@ -114,106 +93,16 @@ module.exports = function(Kartacatalog) {
     });
   }
 
-  // Get shared catalogs
-  Kartacatalog.sharedAll = (email, searchQuery, page, limit, next) => {
-    page = parseInt(page, 10) || 1;
-    limit = parseInt(limit, 10) || 100;
-
-    let search_query = searchQuery ? searchQuery.trim().replace(/[.*+?^${}()|[\]\\]/g, '\\$&') : "";
-
-    const SEARCH_MATCH = {
-      $match: {
-        $or: [
-          {
-            'name': {
-              $regex: search_query,
-              $options: 'i'
-            }
-          }
-        ]
-      }
-    }
-
-    Kartacatalog.getDataSource().connector.connect(function (err, db) {
-      const KartaCatalogCollection = db.collection('karta_catalog');
-      KartaCatalogCollection.aggregate([
-        {
-          $match: { "sharedTo.email": email, $or: [ { "is_deleted": false }, { "is_deleted": { "$exists": false} } ] }
-        },
-        SEARCH_MATCH,
-        USER_LOOKUP_WITH_EMAIL(email),
-        UNWIND_USER,
-        SORT,
-        FACET(page, limit)
-      ]).toArray((err, result) => {
-        if (result) result[0].data.length > 0 ? result[0].metadata[0].count = result[0].data.length : 0;
-        next(err, result);
-      });
-    });
-  }
-
-  // Share catalog to multiple users
-  Kartacatalog.share = (catalog, emails, next) => {
-
-    let catalogId = "";
-    if (catalog.hasOwnProperty("id")) catalogId = karta.id;
-    else catalogId = catalog._id ;
-
-    // Check if any email has already been shared to the catalog or not
-    let duplicateFlag = false;
-    let alreadySharedList = catalog.sharedTo ? catalog.sharedTo.map(x => x.email) : [];
-    let newEmails = emails.filter(email => {
-      if (alreadySharedList.includes(email)) {
-        duplicateFlag = true;
-        return null;
-      } else return email;
-    });
-
-    if (newEmails.length > 0) {
-      // Remove duplicate emails
-      newEmails = [...new Set(newEmails)];
-      // Prepare data for updating in the sharedTo field
-      let data = [];
-      for (let i = 0; i < newEmails.length; i++) {
-        data.push({ email: newEmails[i] });
-      }
-
-      Kartacatalog.update({ "_id": catalogId }, { $addToSet: { "sharedTo": { $each: data } } }, (err) => {
-        if (err) console.log('> error while updating the catalog sharedTo property ', err);
-        else {
-          next(null, "Catalog shared successfully!");
-          // Find existing users in the system
-          Kartacatalog.app.models.user.find({ where: { "email": { inq: newEmails } } }, (err, users) => {
-            if (err) console.log('> error while finding users with emails', err);
-            else {
-              // Prepare notification collection data
-              let notificationData = [];
-              users.forEach(item => {
-                notificationData.push({
-                  title: `${Kartacatalog.app.currentUser.fullName} shared the ${catalog.name}`,
-                  type: "catalog_shared",
-                  contentId: catalog._id,
-                  userId: item.id
-                });
-              });
-              // Insert data in notification collection
-              Kartacatalog.app.models.notification.create(notificationData, (err, result) => {
-                if (err) console.log('> error while inserting data in notification collection', err);
-              });
-            }
-          });
-        }
+  // Share karta catalog to multiple users
+  Kartacatalog.share = (catalogId, userIds, next) => {
+    if (userIds.length > 0) {
+      Kartacatalog.update({ "_id": catalogId }, { $addToSet: { "sharedTo": { $each: userIds } } }, (err, result) => {
+        next(err, "Catalog shared successfully!");
       });
     } else {
-      if (duplicateFlag) {
-        let error = new Error("Can't share a catalog twice to the same user!");
-        error.status = 400;
-        next(error);
-      } else {
-        let error = new Error("Please send an email array");
-        error.status = 400;
-        next(error);
-      }
+      let error = new Error("Please send an userId array");
+      error.status = 400;
+      next(error);
     }
   }
 
@@ -227,4 +116,4 @@ module.exports = function(Kartacatalog) {
       return next(null, "Catalog deleted successfully!");
     })
   }
-};
+}
