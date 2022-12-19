a6a141c5b1eb76fe5fb786c7a21f14e33721c6c5
Tarun.Kethwalia <tarun.kethwalia@otssolutions.com>
2022-12-19 20:01:24 +0530

    Fixed an issue

--- src/server/boot/customRoutes/index.js
+++ src/server/boot/customRoutes/index.js
@@ -15,40 +15,44 @@ module.exports = function (app) {
             email: user.email
         }
 
-        if (user.emailVerified) {
-            // Get company details
-            req.app.models.company.findById(user.companyId.toString(), (err, company) => {
-                if (err) return console.log('> error while fetching company details');
-                user_data.companyLogo = company.logo ? company.logo : "";
-                user_data.profilePic = user.profilePic ? user.profilePic : "";
-                user_data._2faEnabled = user._2faEnabled ? user._2faEnabled : false;
-                user_data.mobileVerified = user.mobileVerified ? user.mobileVerified : false;
-                if (user._2faEnabled && user.mobileVerified) {
-                    let mobileVerificationCode = keygen.number({length: 6});
-                    req.user.updateAttributes({ mobileVerificationCode }, {}, err => {
-                      let twilio_data = {
-                        type: 'sms',
-                        to: user.mobile.e164Number,
-                        from: "+16063667831",
-                        body: `${mobileVerificationCode} is your code for KPI Karta Login.`
-                      }
-                      req.app.models.Twilio.send(twilio_data, function (err, data) {
-                        console.log('> sending code to mobile number:', user.mobile.e164Number);
-                        // if (err) {
-                        //     console.log('> error while sending code to mobile number', err);
-                        //     let error = err;
-                        //     error.status = 500;
-                        //     return next(error);
-                        // }
-                      });
-                    });
-                }
-                res.redirect(`${process.env.WEB_URL}/login?name=${user_data.name}&email=${user_data.email}&userId=${user_data.userId}&access_token=${user_data.accessToken}&profilePic=${user_data.profilePic}&companyLogo=${user_data.companyLogo}&_2faEnabled=${user_data._2faEnabled}&mobileVerified=${user_data.mobileVerified}`);
+        if( user.active && !user.is_deleted ) {
+            if (user.emailVerified) {
+                // Get company details
+                req.app.models.company.findById(user.companyId.toString(), (err, company) => {
+                    if (err) return console.log('> error while fetching company details');
+                    user_data.companyLogo = company.logo ? company.logo : "";
+                    user_data.profilePic = user.profilePic ? user.profilePic : "";
+                    user_data._2faEnabled = user._2faEnabled ? user._2faEnabled : false;
+                    user_data.mobileVerified = user.mobileVerified ? user.mobileVerified : false;
+                    if (user._2faEnabled && user.mobileVerified) {
+                        let mobileVerificationCode = keygen.number({length: 6});
+                        req.user.updateAttributes({ mobileVerificationCode }, {}, err => {
+                          let twilio_data = {
+                            type: 'sms',
+                            to: user.mobile.e164Number,
+                            from: "+16063667831",
+                            body: `${mobileVerificationCode} is your code for KPI Karta Login.`
+                          }
+                          req.app.models.Twilio.send(twilio_data, function (err, data) {
+                            console.log('> sending code to mobile number:', user.mobile.e164Number);
+                            // if (err) {
+                            //     console.log('> error while sending code to mobile number', err);
+                            //     let error = err;
+                            //     error.status = 500;
+                            //     return next(error);
+                            // }
+                          });
+                        });
+                    }
+                    res.redirect(`${process.env.WEB_URL}/login?name=${user_data.name}&email=${user_data.email}&userId=${user_data.userId}&access_token=${user_data.accessToken}&profilePic=${user_data.profilePic}&companyLogo=${user_data.companyLogo}&_2faEnabled=${user_data._2faEnabled}&mobileVerified=${user_data.mobileVerified}`);
+                });
+            } else {
+                req.user.updateAttributes({emailVerified: true}, (err) => {
+                res.redirect(`${process.env.WEB_URL}/sign-up?name=${user_data.name}&email=${user_data.email}&userId=${user_data.userId}&access_token=${user_data.accessToken}`);
             });
+            }
         } else {
-            req.user.updateAttributes({emailVerified: true}, (err) => {
-            res.redirect(`${process.env.WEB_URL}/sign-up?name=${user_data.name}&email=${user_data.email}&userId=${user_data.userId}&access_token=${user_data.accessToken}`);
-        });
+            res.redirect(`${process.env.WEB_URL}/login?isDeleted=${true}&isActive=${false}`);
         }
     });
 
