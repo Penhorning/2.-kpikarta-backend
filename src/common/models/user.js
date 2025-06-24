f6e40446eecad7334d8ccfa66e36a278f6c408c8
3braintechnology <3braintechnology@gmail.com>
2025-06-24 11:46:24 +0530

    Restrict login error message to department admins only when subscription is inactive

--- src/common/models/user.js
+++ src/common/models/user.js
@@ -1337,7 +1337,8 @@ module.exports = function(User) {
           let error = new Error("Your account has been deactivated or deleted by the admin, please connect admin at info@kpikarta.com for more details.");
           error.status = 400;
           next(error);
-        } else if ((user.subscriptionStatus !== "active" && user.subscriptionStatus !== "in_trial") && (user.role()?.name == "user" || user.role()?.name == "department_admin")) {
+        // } else if ((user.subscriptionStatus !== "active" && user.subscriptionStatus !== "in_trial")) {
+        } else if ((user.subscriptionStatus !== "active" && user.subscriptionStatus !== "in_trial") && (user.role()?.name == "department_admin")) {
           let error = new Error("Your cannot login due to subscription cancellation! Please contact to your admin.");
           error.status = 400;
           next(error);
